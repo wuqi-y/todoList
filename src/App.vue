fa971a35c9f6f03a873582538b7ccf3f015b36a5
wuqi-y <2710273092@qq.com>
2023-08-09 16:29:41 +0800

    3Update App.vue
    测试2

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,7 @@ export default {
     add(item) {
       console.log('ojojojojo')
       console.log('111')
+      console.log('222')
       let id = this.todoList.length === 0 ? 100 : this.todoList[this.todoList.length - 1].id + 1
       this.todoList.push({
         id:id,
